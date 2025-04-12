ba73c9a7b8ccfccea28dac30dd023dd33bc001f8
cccaaannn <can.kurt.aa@gmail.com>
2025-04-12 23:01:53 +0300

    tool constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,25 @@
+const ToolLayout = {
+	OrderKey: "Tool-Order",
+	ExpandedKey: "Tool-Expanded",
+	ItemsId: "Tool-Items"
+};
+
+const ToolId = {
+	Base64EncodeDecode: "Tools-Base64EncodeDecode",
+	UrlEncodeDecode: "Tools-UrlEncodeDecode",
+	JwtDecode: "Tools-JwtDecode",
+	JwtEncode: "Tools-JwtEncode",
+	GuidGenerate: "Tools-GuidGenerate"
+};
+
+const History = {
+	Max: 100
+};
+
+const C = {
+	ToolLayout,
+	ToolId,
+	History
+};
+
+export default C;
