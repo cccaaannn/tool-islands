c4e2373dad804aba3014c127e5ef83865a78bba7
cccaaannn <cccaaannn@users.noreply.github.com>
2025-04-12 21:20:31 +0000

    deploy: 36888d4c7582a14ca0eeab71c5157b6b425c1164

--- _astro/index.astro_astro_type_script_index_0_lang.C5kzJScW.js
+++ _astro/index.astro_astro_type_script_index_0_lang.C5kzJScW.js
@@ -1,4 +1,4 @@
-import{C as Qt,S as ue}from"./constants.CCE0qQea.js";/**!
+import{C as Qt,S as ue}from"./constants.Dc4DgJl0.js";/**!
  * Sortable 1.15.6
  * @author	RubaXa   <trash@rubaxa.org>
  * @author	owenm    <owen23355@gmail.com>
---
+++ _astro/resource-utils.C3IEFH3c.js
@@ -0,0 +1,1 @@
+const s="/tool-islands/",t=o=>`${s}${o}`,e=o=>`${s}logos/${o}.svg`,n={get:t,getLogo:e};export{n as R};
