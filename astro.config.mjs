79574124652fce145d3a9fdee4ed58ecc4ce32de
cccaaannn <can.kurt.aa@gmail.com>
2025-06-28 05:47:18 +0300

    preact added

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,23 +7,25 @@ import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 import alpinejs from "@astrojs/alpinejs";
+import preact from "@astrojs/preact";
 
 
 // https://astro.build/config
 export default defineConfig({
-	base: import.meta.env.DEV ? "/" : "/tool-islands/",
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: [{ find: "@", replacement: "/src" }],
-		}
-	},
+    base: import.meta.env.DEV ? "/" : "/tool-islands/",
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: [{ find: "@", replacement: "/src" }],
+        }
+    },
 
-	integrations: [
-		vue(),
-		svelte(),
-		alpinejs(),
-		react({ include: ["**/tools/jwt-encode/*"] }),
-		solidJs({ include: ["**/tools/jwt-decode/*"] })
-	]
+    integrations: [
+      vue(),
+      svelte(),
+      alpinejs(),
+      react({ include: ["**/tools/jwt-encode/*"] }),
+      solidJs({ include: ["**/tools/jwt-decode/*"] }),
+      preact({ include: ["**/tools/str-escape/*"] })
+    ]
 });
